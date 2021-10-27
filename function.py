c11bf7b1e3819d6e49b7d925c64b5d6e6c34544b
ABHINAV VENKATARAMANI <91325345+Abhinav2711@users.noreply.github.com>
2021-10-27 08:18:37 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+# functions
+#sub unit
+def test():
+    r=45
+    print("hi from function")
+    print(r)
+#main unit or program
+print("example for function")
+test()
+print("after 1st call")
+test()
+print("after 2nd call")
+test()
